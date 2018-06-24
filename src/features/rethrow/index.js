7e4febf2e0fcbd467f262e691e9e4e31214c58f6
Wilson Miao Liu <lockleeliumiaowilson@gmail.com>
2018-06-24 17:27:48 +1000

    add support to exception rethrow

---
+++ src/features/rethrow/index.js
@@ -0,0 +1,39 @@
+const _ = require('lodash');
+const AST = require('../../ast');
+const getValue = require('../../valueProvider');
+
+const Rethrow = {
+    accept: ({ current, parent, }) => {
+        const accepted =
+            current.node === 'MethodDeclaration' &&
+            AST.hasAnnotation(current.modifiers, 'rethrow');
+        return accepted;
+    },
+
+    run: ({ current, parent, root, }) => {
+        const annotation = _.find(current.modifiers, modifier => modifier.node === 'Annotation' && getValue(modifier.typeName) === 'rethrow');
+        if(!annotation.value) {
+            throw new Error('value is required for @rethrow');
+        }
+
+        const rethrowException = getValue(annotation.value);
+
+        const newCode = `try {
+        }
+        catch(Exception e) {
+            System.debug(LoggingLevel.Error, e.getStackTraceString());
+            throw new ${rethrowException}(e.getMessage());
+        }`;
+
+        const newNode = AST.parseBlockStatement(newCode);
+        newNode.body.statements = [
+            ...current.body.statements
+        ];
+
+        AST.removeChildren(current.body, 'statements');
+        AST.appendChild(current.body, 'statements', newNode);
+        AST.removeChild(current, 'modifiers', annotation);
+    },
+};
+
+module.exports = Rethrow;
